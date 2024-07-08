2d5478e0b592c14e379ec2e8c4cabb46857a7755
mikheyev9 <mikheyev9@gmail.com>
2024-07-08 14:17:59 +0300

    create session for all requests, and add TCPConnector

--- aiohttp_requests/request.py
+++ aiohttp_requests/request.py
@@ -5,26 +5,27 @@ import time
 import aiohttp
 
 async def check_website(url,
-                        retries_in_repeated_requests=3):
+                        session,
+                        retries_in_repeated_requests=3,
+                        delay_wait_before_start_retrying=3):
     retries = retries_in_repeated_requests
-    delays = [i + 1 for i in range(retries)]
-    async with aiohttp.ClientSession() as session:
-        for attempt in range(retries):
-            start_time = time.time()
-            checked_at = datetime.now(timezone.utc).isoformat()
-            try:
-                async with session.get(url, timeout=10) as response:
-                    response_time = time.time() - start_time
-                    status = response.status
-                    error = None
-                    if status == 200:
-                        return url, status, response_time, checked_at, error
-            except Exception as ex:
+    delays = [i + delay_wait_before_start_retrying for i in range(retries)]
+    for attempt in range(retries):
+        start_time = time.time()
+        checked_at = datetime.now(timezone.utc).isoformat()
+        try:
+            async with session.get(url, timeout=15) as response:
                 response_time = time.time() - start_time
-                status = 'Exception'
-                error = str(ex)
-                await asyncio.sleep(delays[attempt])
-            return url, status, response_time, checked_at, error
+                status = response.status
+                error = None
+                if status == 200:
+                    return url, status, response_time, checked_at, error
+        except Exception as ex:
+            response_time = time.time() - start_time
+            status = 'Exception'
+            error = str(ex)
+            await asyncio.sleep(delays[attempt])
+        return url, status, response_time, checked_at, error
 
 
 # Пример использования функции
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import aiohttp
 import os
 from datetime import datetime, timedelta, timezone
 
@@ -21,20 +22,27 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_DATABASE = os.getenv('DB_DATABASE')
 class UptimeMonitor:
     def __init__(self, token, chat_id, db_connection,
+                 need_saving_in_local_db=False,
                  interval_between_checking=800,
                  time_wait_before_retrying=80,
-                 delay_wait_before_retrying=5,
-                 retries_in_repeated_requests=3):
+                 delay_wait_before_start_retrying=5,
+                 retries_in_repeated_requests=3,
+                 pool_size=100,
+                 limit_per_host=4):
         self.token = token
         self.chat_id = chat_id
         self.db = Database()
+        self.need_saving_in_local_db = need_saving_in_local_db
         self.telegram_bot = TelegramBot(token=self.token, channel_id=self.chat_id)
         self.db_connection = db_connection
         self.urls = []
+
         self.INTERVAL_BETWEEN_CHECKING = interval_between_checking
         self.TIME_WAIT_BEFORE_RETRYING = time_wait_before_retrying
-        self.DELAY_WAIT_BEFORE_START_RETRYING = delay_wait_before_retrying
+        self.DELAY_WAIT_BEFORE_START_RETRYING = delay_wait_before_start_retrying
         self.RETRIES_IN_REPEATING_REQUESTS = retries_in_repeated_requests
+        self.POOL_SIZE = pool_size
+        self.LIMIT_PER_HOST = limit_per_host
         self.down_since = {}
 
     def _create_success_message(self, url, downtime):
@@ -59,8 +67,18 @@ class UptimeMonitor:
             downtime = timedelta(0)
         return str(downtime).split('.')[0]
 
+    async def log_status_in_sqlite(self, url, status, response_time, checked_at):
+        if self.need_saving_in_local_db:
+            await self.db.log_status(url, status, response_time, checked_at)
+    async def create_session(self):
+        connector = aiohttp.TCPConnector(limit=self.POOL_SIZE,
+                                         limit_per_host=self.LIMIT_PER_HOST)
+        session = aiohttp.ClientSession(connector=connector)
+        return session
+
     async def check_site_until_up(self, url):
         self.down_since.setdefault(url, datetime.now(timezone.utc))
+        session = await self.create_session()
         current_wait_time = 0
         await asyncio.sleep(self.DELAY_WAIT_BEFORE_START_RETRYING)
 
@@ -74,12 +92,14 @@ class UptimeMonitor:
 
                 url, status, response_time, checked_at, error = await check_website(
                     url,
-                    self.RETRIES_IN_REPEATING_REQUESTS
+                    session,
+                    self.RETRIES_IN_REPEATING_REQUESTS,
+                    self.DELAY_WAIT_BEFORE_START_RETRYING
                 )
+                await self.log_status_in_sqlite(url, status, response_time, checked_at)
 
                 if status == 200:
                     downtime = self._calculate_downtime(url)
-                    await self.db.log_status(url, status, response_time, checked_at)
                     success_message = self._create_success_message(url, downtime)
                     self.telegram_bot.add_to_queue(success_message)
                     logger.info(f"{url} is back up. Downtime: {downtime}")
@@ -89,7 +109,9 @@ class UptimeMonitor:
                     error_message = self._create_error_message(url, status, error)
                     self.telegram_bot.add_to_queue(error_message)
                     await asyncio.sleep(self.TIME_WAIT_BEFORE_RETRYING + current_wait_time)
-                    current_wait_time += 5
+                    current_wait_time += self.DELAY_WAIT_BEFORE_START_RETRYING
+                    logger.info(f"{url} {status} {response_time} {checked_at} {error if error else ''}")
+
 
             except Exception as e:
                 logger.error(f"An error occurred while checking {url}: {str(e)}")
@@ -97,38 +119,45 @@ class UptimeMonitor:
                 self.telegram_bot.add_to_queue(exception_message)
                 await asyncio.sleep(self.TIME_WAIT_BEFORE_RETRYING + current_wait_time)
                 current_wait_time += 5
+        await session.close()
 
-    async def process_website_check(self, url):
+    async def process_website_check(self, url, session):
         try:
             url, status, response_time, checked_at, error = await check_website(
                 url,
-                self.RETRIES_IN_REPEATING_REQUESTS
+                session,
+                self.RETRIES_IN_REPEATING_REQUESTS,
+                self.DELAY_WAIT_BEFORE_START_RETRYING
             )
             if status != 200:
-                await self.db.log_status(url, status, response_time, checked_at)
                 error_message = self._create_error_message(url, status, error)
                 self.telegram_bot.add_to_queue(error_message)
                 if url not in self.down_since:
                     asyncio.create_task(self.check_site_until_up(url))
-            else:
-                await self.db.log_status(url, status, response_time, checked_at)
-            logger.info(f"{url} {status} {response_time} {checked_at}")
+            await self.log_status_in_sqlite(url, status, response_time, checked_at)
+            logger.info(f"{url} {status} {response_time} {checked_at} {error if error else ''}")
 
         except Exception as e:
             logger.error(f"An error occurred while processing {url}: {str(e)}")
             exception_message = self._create_exception_message(url, e)
             self.telegram_bot.add_to_queue(exception_message)
 
+    async def check_all_websites(self):
+        session = await self.create_session()
+        tasks = [self.process_website_check(url, session) for url in self.urls]
+        await asyncio.gather(*tasks)
+        await session.close()
+        print('session closed')
 
     async def uptime_check(self):
         while True:
             self.urls = await self.db_connection.get_sites()
-            tasks = [self.process_website_check(url) for url in self.urls]
-            await asyncio.gather(*tasks)
+            await self.check_all_websites()
             await asyncio.sleep(self.INTERVAL_BETWEEN_CHECKING)
 
     async def main(self):
-        await self.db.init_db()
+        if self.need_saving_in_local_db:
+            await self.db.init_db()
         asyncio.create_task(self.uptime_check())
         await self.telegram_bot.start_polling()
 
@@ -147,8 +176,11 @@ if __name__ == '__main__':
         token=TOKEN,
         chat_id=CHAT_ID,
         db_connection=db_connection,
+        need_saving_in_local_db=False,
         interval_between_checking=800,
         time_wait_before_retrying=80,
-        delay_wait_before_retrying=5
+        delay_wait_before_start_retrying=15,
+        pool_size=50,
+        limit_per_host=3
     )
     asyncio.run(monitor.main())
