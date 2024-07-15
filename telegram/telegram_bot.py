40cf9ddbb3e3a65f5d650e0357730cec779015b0
mikheyev9 <mikheyev9@gmail.com>
2024-07-15 13:07:56 +0300

    telegram bot update

--- telegram/telegram_bot.py
+++ telegram/telegram_bot.py
@@ -1,14 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher, html
 from aiogram.exceptions import TelegramRetryAfter, TelegramAPIError
-from collections import deque
 
 class TelegramBot:
     def __init__(self, token, channel_id, initial_delay=1):
         self.bot = Bot(token=token)
         self.dp = Dispatcher()
         self.channel_id = channel_id
-        self.message_queue = deque()
+        self.message_queue: asyncio.Queue[str] = asyncio.Queue()
         self.delay = initial_delay
 
     async def send_message(self, message):
@@ -16,24 +15,21 @@ class TelegramBot:
             await self.bot.send_message(chat_id=self.channel_id, text=html.quote(message))
             self.delay = 1  # Reset delay after successful send
         except TelegramRetryAfter as e:
-            print('TelegramRetryAfter', e)
             # Too many requests, need to wait
             self.delay = e.retry_after + 2
-            self.message_queue.appendleft(message)  # Re-add the message to the front of the queue
+            await self.message_queue.put(message)  # Re-add the message to the front of the queue
         except TelegramAPIError as e:
             # Other API errors
-            self.message_queue.appendleft(message)  # Re-add the message to the front of the queue
-            print(f"Failed to send message: {e}")
+            await self.message_queue.put(message)  # Re-add the message to the front of the queue
 
     async def process_queue(self):
         while True:
-            if self.message_queue:
-                message = self.message_queue.popleft()
-                await self.send_message(message)
+            message = await self.message_queue.get()
+            await self.send_message(message)
             await asyncio.sleep(self.delay)  # Delay between messages
 
-    def add_to_queue(self, message):
-        self.message_queue.append(message)
+    async def add_to_queue(self, message: str):
+        await self.message_queue.put(message)
 
     async def start_polling(self):
         asyncio.create_task(self.process_queue())
@@ -47,7 +43,7 @@ async def test_TelegramRetryAfter():
     CHAT_ID = os.getenv('CHAT_ID')
     telegram_bot = TelegramBot(token=TOKEN, channel_id=CHAT_ID)
     for i in range(1000):
-        telegram_bot.add_to_queue(f"Test message {i}")
+        await telegram_bot.add_to_queue(f"Test message {i}")
     await telegram_bot.start_polling()
 
 if __name__ == '__main__':
